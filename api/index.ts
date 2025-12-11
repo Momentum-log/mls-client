d349c40936e7af12eb6f0605e0f97817197ca0aa
thekiwidev <adedotungabriel20@gmail.com>
2025-12-11 17:23:11 +0100

    chore: Update API base URL to deployed server.

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://mls-server-omoq.onrender.com/api";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
